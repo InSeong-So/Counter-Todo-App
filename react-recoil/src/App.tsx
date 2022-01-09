06ce4e825802552c9bc2f5d8d20202a2a7944fda
InSeongSo <goflvhxj2547@gmail.com>
2022-01-09 23:28:43 +0900

    ✒️Feat: React-Recoil 카운터 앱 추가

---
+++ react-recoil/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Wrapper from './components/Wrapper';
+import ViewCounter from './components/ViewCounter';
+import { RecoilRoot } from 'recoil';
+
+function App() {
+  return (
+    <RecoilRoot>
+      <div className="App">
+        <nav>
+          <ViewCounter />
+        </nav>
+        <Wrapper />
+      </div>
+    </RecoilRoot>
+  );
+}
+
+export default App;
---
+++ react-recoil/src/components/Wrapper.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Counter, CountMultTwo } from './Counter';
+
+const Wrapper = () => {
+    return (
+        <>
+            <Counter />
+            <CountMultTwo />
+        </>
+    )
+}
+
+
+export default Wrapper;
