4fb75244b4b70ac96f43c6ab24177a8fccadce4f
InSeongSo <goflvhxj2547@gmail.com>
2022-02-21 19:36:20 +0900

    ✒️Feat: react-recoil counter 개발

--- packages/react-recoil/src/App.tsx
+++ packages/react-recoil/src/App.tsx
@@ -1,17 +1,71 @@
-import Wrapper from './components/Wrapper';
-import ViewCounter from './components/ViewCounter';
-import { RecoilRoot } from 'recoil';
+import * as Styled from '@project/ui/src/react';
+import { Suspense } from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { asyncCounterSelector, counterState } from 'recoil/count';
+
+const ids = [
+  { button: 'increment', icon: 'fa fa-plus' },
+  { button: 'decrement', icon: 'fa fa-minus' },
+  { button: 'reset', icon: 'fa fa-refresh' },
+];
 
 function App() {
-  return (
-    <RecoilRoot>
-      <div className="App">
-        <nav>
-          <ViewCounter />
-        </nav>
-        <Wrapper />
+  const [count, setCount] = useRecoilState<number>(counterState);
+  const setAsyncCount = useSetRecoilState<number>(asyncCounterSelector);
+
+  const eventType: Record<string, () => void> = {
+    'increment-button': () => setCount(count => count + 1),
+    'decrement-button': () => setCount(count => count - 1),
+    'reset-button': () => setCount(0),
+    'increment-button-async': () => setAsyncCount(count => count + 1),
+    'decrement-button-async': () => setAsyncCount(count => count - 1),
+    'reset-button-async': () => setAsyncCount(0),
+  };
+
+  const handleSyncClick = (event: React.MouseEvent) => {
+    const target = event.target as HTMLElement;
+    const type = target.tagName === 'I' ? (target.parentElement as HTMLElement).id : target.id;
+
+    eventType[type]();
+  };
+
+  const handleAsyncClick = (event: React.MouseEvent) => {
+    const target = event.target as HTMLElement;
+    const type = target.tagName === 'I' ? (target.parentElement as HTMLElement).id : target.id;
+
+    eventType[type]();
+  };
+
+  const Fallback = () => {
+    return (
+      <div id="load">
+        <span>Loading...</span>
       </div>
-    </RecoilRoot>
+    );
+  };
+
+  return (
+    <Styled.Counter>
+      <Suspense fallback={<Fallback />}>
+        <Styled.CounterDisplay>{count}</Styled.CounterDisplay>
+        <Styled.CounterTypeTitle>Sync</Styled.CounterTypeTitle>
+        <Styled.CounterButtons>
+          {ids.map(({ button, icon }, index) => (
+            <Styled.Button id={`${button}-button`} key={index} onClick={handleSyncClick}>
+              <i className={icon}></i>
+            </Styled.Button>
+          ))}
+        </Styled.CounterButtons>
+        <Styled.CounterTypeTitle>Async</Styled.CounterTypeTitle>
+        <Styled.CounterButtons>
+          {ids.map(({ button, icon }, index) => (
+            <Styled.Button id={`${button}-button-async`} key={index} onClick={handleAsyncClick}>
+              <i className={icon}></i>
+            </Styled.Button>
+          ))}
+        </Styled.CounterButtons>
+      </Suspense>
+    </Styled.Counter>
   );
 }
 
--- packages/react-recoil/src/index.tsx
+++ packages/react-recoil/src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
 import App from './App';
+import { Global } from '@emotion/react';
+import { reset } from '@project/ui/src/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Global styles={reset} />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
 );
