682aab3faa17091386487730520639c202964aa5
InSeong-So <goflvhxj2547@gmail.com>
2022-08-12 23:40:45 +0900

    🔬Chore: 프로젝트 셋업

--- packages/vanilla/__tests__/vanilla.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const vanilla = require('..');
-
-describe('vanilla', () => {
-    it('needs tests');
-});
--- packages/vanilla/lib/vanilla.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = vanilla;
-
-function vanilla() {
-    // TODO
-}
